e4f904ef7f67a3d51a56def334dec2f359a77f1e
Rohit-Pakhre <pakhrerohit281@gmail.com>
2025-08-07 18:49:54 +0530

    Style: Counter UI done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Counter from "./components/Counter";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="min-h-screen bg-neutral-800 flex items-center justify-center">
+      <Counter />
+    </main>
+  );
 };
 
 export default App;
